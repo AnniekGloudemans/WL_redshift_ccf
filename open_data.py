ce83cd54c0dc1dc8edc065dd02799ab12c392e37
AnniekGloudemans <anniekgloudemans@gmail.com>
2022-09-20 16:09:24 +0200

    fixed bug

--- open_data.py
+++ open_data.py
@@ -54,6 +54,35 @@ def input_redshifts_match_weaveio(target_table, input_table):
 	return np.array(input_redshifts)
 
 
+def match_blue_red_spectra(table_blue, table_red):
+	"""
+	Sort blue and red spectra to same target order
+
+	Keyword arguments:
+	table_blue/red (table) -- Table with all spectra information
+
+	Return:
+	table_blue/red (table) -- Sorted/matched table with all spectra information 
+
+	"""
+
+    if len(table_blue) == len(table_red):
+        return table_blue, table_red
+    
+    idx_red_list = []
+    idx_blue_list = []
+
+    for i in range(len(table_blue)):
+        if table_blue['targname'][i] in table_red['targname']:
+            idx_red = np.where(table_red['targname'] == table_blue['targname'][i])[0]
+
+            idx_blue_list.append(i)
+            idx_red_list.append(idx_red[0])
+
+    return table_blue[np.array(idx_blue_list)], table_red[np.array(idx_red_list)]
+
+
+
 def input_redshifts_match(target_table, input_table):
 	"""
 	Find input redshifts 
